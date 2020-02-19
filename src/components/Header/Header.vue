8ce2ccea947ee52c3aadea3f23a635ad6fe952d9
hanlianhao <haoye999han@gmail.com>
2020-02-19 21:28:13 +0800

    init commit

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="header">header</header>
+</template>
+
+<script>
+export default {
+  name: 'header',
+};
+</script>
+
+<style lang="less" scoped>
+.header {
+  height: 100px;
+  border-bottom: 1px solid #eee;
+}
+</style>
